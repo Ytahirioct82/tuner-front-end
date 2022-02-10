c471ba687db667351cd0bcb98d272e5dfd896b2f
Younes Tahiri <you@example.com>
2022-02-09 19:49:49 -0500

    added rout to all artist songs

--- src/Components/Song.js
+++ src/Components/Song.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
 
 function Song() {
-  const navigate = useNavigate();
+  const nav = useNavigate();
   const URL = process.env.REACT_APP_BASE_URL;
   const [songs, setSongs] = useState([]);
   const { id } = useParams();
@@ -15,9 +15,9 @@ function Song() {
       })
       .catch((err) => {
         console.log(err);
-        navigate("/");
+        nav("/");
       });
-  }, [URL, id, navigate]);
+  }, [URL, id, nav]);
 
   return (
     <div>
